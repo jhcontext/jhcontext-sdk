e76c7af0c482d0a41616cd259e7c056322d2636f
selfprofx <124059827+selfprofx@users.noreply.github.com>
2026-04-19 10:13:30 -0300

    Bump version to 0.3.5

--- jhcontext/__init__.py
+++ jhcontext/__init__.py
@@ -54,7 +54,7 @@ from .semantics import observation, interpretation, situation, userml_payload
 from .forwarding import ForwardingEnforcer
 from .persistence import StepPersister
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 
 __all__ = [
     # Models
